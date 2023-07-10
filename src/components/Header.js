83fe77df5b07e3739fb25aa0f75fe21f28c285ea
Andrés Camilo Arango Calle <andres.arango@est.iudigital.edu.co>
2023-07-10 02:28:52 -0500

    news IUD

---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function Header({categories=[], getNews}) {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <a className="navbar-brand" href="#">Noticias IUD</a>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Link</a>
+                            </li>
+                            <li className="nav-item">
+                                <select
+                                    onChange={getNews}
+                                    className="form-select"
+                                    aria-label="Default select example">
+                                    <option value='selected'>Selecciona una categoría</option>
+                                    {
+                                        categories.map(categoria => {
+                                            return (
+                                                <option key={categoria} value={categoria}>{categoria}</option>
+                                            )
+                                        })
+                                    }
+
+                                </select>
+                            </li>
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+  )
+}
---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const API_URL='https://newsapi.org/v2'
+
+export const AUTOR='Andres Arango'
+
+export const API_COUNTRY='co'
+
+
+//https://newsapi.org/v2/top-headlines?country=us&apiKey=821a6badd6f74a0e95a6f0ee1d476bc1&category=health
